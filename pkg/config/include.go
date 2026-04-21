cfc56dbb932483ec2e591546902dcaffd276c002
Carlos Alexandro Becker <caarlos0@users.noreply.github.com>
2026-04-20 22:51:44 -0300

    fix: clean include.go
    Signed-off-by: Carlos Alexandro Becker <caarlos0@users.noreply.github.com>

--- pkg/config/include.go
+++ pkg/config/include.go
@@ -1,63 +1,14 @@
 package config
 
 import (
-	"context"
-	"fmt"
 	"io"
-	"net/http"
 	"os"
-	"strings"
-	"time"
-
-	"github.com/avast/retry-go/v4"
 )
 
 // IncludeFromURL allows to import a config from an URL, passing headers if needed (e.g. authorization).
 type IncludeFromURL struct { // pro only
 	URL     string            `yaml:"url" json:"url"`
 	Headers map[string]string `yaml:"headers,omitempty" json:"headers,omitempty"`
-}
-
-// Reader ...
-func (i IncludeFromURL) Reader() (io.ReadCloser, error) {
-	url := i.URL
-	if !strings.HasPrefix(url, "http") && strings.Contains(url, "/") { // assume it is a path within github
-		// TODO: maybe deprecate this? it doesn't really save much space...
-		url = fmt.Sprintf("https://raw.githubusercontent.com/%s", i.URL)
-	}
-	//nolint:noctx
-	req, err := http.NewRequest(http.MethodGet, url, nil)
-	if err != nil {
-		return nil, err
-	}
-	for k, v := range i.Headers {
-		req.Header.Add(k, os.ExpandEnv(v))
-	}
-	var body io.ReadCloser
-	err = retry.Do(func() error {
-		resp, err := http.DefaultClient.Do(req)
-		if err != nil {
-			return err
-		}
-		if resp.StatusCode >= 500 || resp.StatusCode == http.StatusTooManyRequests {
-			resp.Body.Close()
-			return fmt.Errorf("%s %q: %s", "Get", i.URL, resp.Status)
-		}
-		if http.StatusOK != resp.StatusCode {
-			resp.Body.Close()
-			return retry.Unrecoverable(fmt.Errorf("%s %q: %s", "Get", i.URL, resp.Status))
-		}
-		body = resp.Body
-		return nil
-	},
-		retry.Context(context.Background()),
-		retry.Attempts(3),
-		retry.Delay(2*time.Second),
-		retry.MaxDelay(15*time.Second),
-		retry.DelayType(retry.BackOffDelay),
-		retry.LastErrorOnly(true),
-	)
-	return body, err
 }
 
 // IncludeFromFile allows to import a config from another file.
@@ -85,22 +36,5 @@ func (i Include) String() string {
 	return i.FromURL.URL
 }
 
-// Load the included content.
-func (i Include) Load() (io.ReadCloser, error) {
-	if i.FromFile.Path != "" {
-		return os.Open(i.FromFile.Path)
-	}
-
-	if i.FromURL.URL != "" {
-		return i.FromURL.Reader()
-	}
-
-	return io.NopCloser(strings.NewReader(i.Content)), nil
-}
-
 // IncludedMarkdown type alias.
 type IncludedMarkdown Include
-
-func (i IncludedMarkdown) Load() (io.ReadCloser, error) {
-	return Include(i).Load()
-}
